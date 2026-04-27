089de692c061af40498046a6bff866cc978a36c3
Paulo Gabriel <pgabrieluto@gmail.com>
2026-04-27 15:48:06 -0300

    Refatoração: Renomeia de componentes de estilo e remoção de arquivos obsoletos

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -12,17 +12,6 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 
 document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("botão-extensão").addEventListener("click", function() {
-        const section = document.getElementById("section-tres");
-        if (section.style.display === "none") {
-          section.style.display = "block";
-        } else {
-          section.style.display = "none";
-        }
-      });
-});
-
-document.addEventListener("DOMContentLoaded", function(){
     const botao1 = document.getElementById("botao-section-tres");
 
     botao1.addEventListener("click", function(){
