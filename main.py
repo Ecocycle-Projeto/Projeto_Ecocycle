50ea632679bca1c5816e9be9d2399b6d7281857b
Paulo Gabriel <pgabrieluto@gmail.com>
2026-06-20 09:44:10 -0300

    preparando para deploy

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ load_dotenv()
 
 # Configuração da aplicação Flask
 app = Flask(__name__)
-CORS(app, supports_credentials=True, resources={r"/api/*": {"origins": "http://127.0.0.1:5000"}})
+CORS(app, supports_credentials=True, resources={r"/api/*": {"origins": "*"}})
 app.config["JSON_SORT_KEYS"] = False
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(minutes=15)
 app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=7)
@@ -34,7 +34,8 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["RECAPTCHA_SITE_KEY"] = os.getenv('RECAPTCHA_SITE_KEY')
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 app.config["MAIL_SERVER"] = os.getenv("MAIL_SERVER")
-app.config["MAIL_PORT"] = int(os.getenv("MAIL_PORT", 587))
+mail_port_env = os.getenv("MAIL_PORT")
+app.config["MAIL_PORT"] = int(mail_port_env) if mail_port_env else 587
 app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
 app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
 db.init_app(app)
